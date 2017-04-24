708e71697f8ee26e65687791e8647b792851da71
Eyeball Edward <edwardyeung39@gmail.com>
2017-04-24 23:28:31 +0800

    Example to complement bus services API

---
+++ examples/BusServices-Example.js
@@ -0,0 +1,9 @@
+const BusServiceAPI = require('..').BusServiceAPI,
+	prettySeconds = require('pretty-seconds');
+
+var api = new BusServiceAPI(require('./read-config')());
+
+api.getBusServicesOperatingNow(services => {
+	for (let service of services)
+		console.log(`Service ${service.serviceNo} is operating now under ${service.operator}`);
+});
