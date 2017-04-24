0f99d9891c757bde9e0a7fc453f272e37f4b9e7c
Eyeball Edward <edwardyeung39@gmail.com>
2017-04-24 23:36:51 +0800

    Add in direction for service API

--- examples/BusServices-Example.js
+++ examples/BusServices-Example.js
@@ -5,5 +5,5 @@ var api = new BusServiceAPI(require('./read-config')());
 
 api.getBusServicesOperatingNow(services => {
 	for (let service of services)
-		console.log(`Service ${service.serviceNo} is operating now under ${service.operator}`);
+		console.log(`Service ${service.serviceNo} is operating now under ${service.operator} on direction ${service.direction}`);
 });
